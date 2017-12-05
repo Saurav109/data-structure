badb1f8b0c630e051e164992a70afba57563a84e
saurav109 <scientistboy109@gmail.com>
2017-12-05 23:52:29 +0600

    illigal

---
+++ illegal.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+  int x=10;
+  go(&x);
+  printf("%d\n",x );
+ return 0;
+}
+
+void go(int *a) {
+  *a=40;
+  *(a+1)=59;
+  //this will work but its illigal way of creating variable
+  printf("printng other value %d\n",*(a+1) );
+}
