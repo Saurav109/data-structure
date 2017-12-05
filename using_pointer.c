ce354e2e38b23baa53c9bfc91a4b2824cadada60
saurav109 <scientistboy109@gmail.com>
2017-12-06 00:27:22 +0600

    adding new file

---
+++ using_pointer.c
@@ -0,0 +1,47 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+//global variable
+//pointer for stroing address
+int *a;
+//variable for tracking ammount numbers
+int n=0;
+
+
+
+//declearing function
+void pr();
+void inputs(int o);
+
+
+int main(){
+//allocatign  a memory for pointer
+  a= malloc (sizeof(int));
+
+
+  int i,num;
+//lop for getting value
+  for(i=0;i<5;i++){
+    scanf("%d",&num );
+    inputs(num);
+  }
+  printf("%s\n\n","" );
+//printing value
+  pr();
+ return 0;
+}
+
+
+void pr(){
+  int z=0;
+//looping to get at the end , where (maybe) adress will be NULL
+  while(*(a+z)!='\0'){
+    printf("%d ",*(a+z) );
+    z++;
+  }
+}
+void inputs(int o){
+  *(a+n)=o;
+  n++;
+}
