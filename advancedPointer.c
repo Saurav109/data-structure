1c057787a71ec5415938816fc5b1abb39b01bb60
saurav109 <scientistboy109@gmail.com>
2017-12-07 02:29:04 +0600

    trying to understand advance pointer

--- advancedPointer.c
+++ advancedPointer.c
@@ -1,11 +1,39 @@
+
 #include<stdio.h>
 
 int main(){
-  int *c[4]={1,2,3,5};
-  int **cp[4]={c+0,c+1,c+2,c+3};
-  int ***cpp[]=cp;
+//three pointer
+  int *x;
+  int **y;
+  int ***z;
 
-  printf("%d\n",*++cpp );
+
+  int a=10;
+//x holding the adress of a
+  x=&a;
+//y is holding the adress of pointer x
+  y=x;
+//z is holding the adress of pointer of y
+  z=y;
+
+//*x is value of a cz its holds a's adress
+//y holdes the adress of x , where in x holdes adress of a
+//so y holds the adress of a, so *y is the value of a
+//same for the *z
+  printf("%d %d %d %d\n",a,*x,*y,*z );
+
+  printf("%p %p %p\n",x,y,z );
+
+//need someone to explaine this ??!!
+  char *c[] = {"GeksQuiz", "MCQ", "TEST", "QUIZ"};
+  char **cp[] = {c+3, c+2, c+1, c};
+  char ***cpp = cp;
+
+//???
+     printf("%s ", **++cpp);
+     printf("%s ", *--*++cpp+3);
+     printf("%s ", *cpp[-2]+3);
+     printf("%s ", cpp[-1][-1]+1);
 
  return 0;
 }
